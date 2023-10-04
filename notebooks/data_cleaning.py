403dff764681b27c6c96156df49e6baacaf576c1
mathieupelo <mathieu-peloquin@hotmail.com>
2023-10-04 14:01:27 -0400

    Started data_cleaning

--- notebooks/data_cleaning.py
+++ notebooks/data_cleaning.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import ast
 
 
 
@@ -7,10 +7,36 @@ def main():
     print("READING JSON")
     csv_path = "../ift6758/data/data_saison_2016_2017_play_by_play.json"
     df = pd.read_json(csv_path)
-    df.head()
-
     print(df)
 
+
+    df['event'] = df[0].apply(lambda x: x['result']['event'])
+    df['eventCode'] = df[0].apply(lambda x: x['result']['eventCode'])
+    df['eventTypeId'] = df[0].apply(lambda x: x['result']['eventTypeId'])
+    df['description'] = df[0].apply(lambda x: x['result']['description'])
+
+    df["eventIdx"] = df[0].apply(lambda x: x["about"]["eventIdx"])
+    df["eventId"] = df[0].apply(lambda x: x["about"]["eventId"])
+
+    df['period'] = df[0].apply(lambda x: x['about']['period'])
+    df['periodType'] = df[0].apply(lambda x: x['about']['periodType'])
+
+    df['ordinalNum'] = df[0].apply(lambda x: x['about']['ordinalNum'])
+
+    df['periodTime'] = df[0].apply(lambda x: x['about']['periodTime'])
+    df['periodTimeRemaining'] = df[0].apply(lambda x: x['about']['periodTimeRemaining'])
+    df['dateTime'] = df[0].apply(lambda x: x['about']['dateTime'])
+    df['goals_away'] = df[0].apply(lambda x: x['about']['goals']['away'])
+    df['goals_home'] = df[0].apply(lambda x: x['about']['goals']['home'])
+
+    #df['coordinatesX'] = df[0].apply(lambda x: x['coordinates']['x'])
+    #df['coordinatesY'] = df[0].apply(lambda x: x['coordinates']['y'])
+
+
+    df = df[['event', 'eventCode', 'eventTypeId', 'description', 'eventIdx', 'eventId','period', 'periodType', 'periodTime',
+             'periodTimeRemaining', 'dateTime', 'goals_away', 'goals_home']]
+    print(df.columns)
+    print(df)
 
 
 
