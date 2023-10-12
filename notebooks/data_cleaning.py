8eecbd1ebcd5c7d3279eb9603c65ca7e2f4b6147
mathieupelo <mathieu-peloquin@hotmail.com>
2023-10-12 10:26:55 -0400

    Added Faceoff Table

--- notebooks/data_cleaning.py
+++ notebooks/data_cleaning.py
@@ -42,10 +42,43 @@ def create_event_df(data):
                 pass
 
     event_df = pd.concat([event_df, pd.DataFrame(new_rows)], ignore_index=True)
-
-    print(event_df)
     event_df.to_csv("../ift6758/data_clean/eventdf_clean.csv", index=False)
 
+def create_faceoff_df(data):
+    # We create an empty DataFrame
+    columns = ['event']
+    faceoff_df = pd.DataFrame(columns=columns)
+
+    # We create new entries
+    # new_row = {'Column1': 'Value1', 'Column2': 'Value2'}
+
+    new_rows = []
+    # for every entry in df
+    for entry in data:
+        for line in data[entry]:
+            try:
+                if line["result"]["eventTypeId"] == "FACEOFF":
+                    event = line['result']['event']
+                    eventCode = line['result']['eventCode']
+                    eventTypeId = line['result']['eventTypeId']
+                    description = line['result']['description']
+                    dateTime = line['about']['dateTime']
+                    period = line['about']['period']
+                    periodType = line['about']['periodType']
+                    goalsHome = line['about']['goals']['home']
+                    goalsAway = line['about']['goals']['away']
+                    winner = line['players'][0]["player"]["fullName"]
+                    loser = line['players'][1]["player"]["fullName"]
+                    new_row = {'event': event, 'eventCode':eventCode, 'eventTypeId': eventTypeId,
+                               'description':description, 'dateTime': dateTime, 'period': period,
+                               'periodType': periodType, 'goalsHome': goalsHome, 'goalsAway': goalsAway,
+                               'winner': winner, 'loser': loser}
+                    new_rows.append(new_row)
+            except:
+                pass
+
+    faceoff_df = pd.concat([faceoff_df, pd.DataFrame(new_rows)], ignore_index=True)
+    faceoff_df.to_csv("../ift6758/data_clean/faceoffdf_clean.csv", index=False)
 
 def main():
 
@@ -53,7 +86,10 @@ def main():
     json_path = "../ift6758/data/data_total_play_by_play.json"
 
     data = pd.read_json(json_path)
+
     create_event_df(data)
+    create_faceoff_df(data)
+
 
     """
 
